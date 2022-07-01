692469eab10727be7341d9eac5a5293c6ce1c9d1
cnstll <calle@student.42.fr>
2022-07-01 18:30:24 +0200

    refacto and check param added | still need precision check

--- ml_01/ex05/z-score.py
+++ ml_01/ex05/z-score.py
@@ -1,41 +1,43 @@
 import numpy as np
 import math
 
+
 def check_param(x):
+    if not isinstance(x, np.ndarray):
+        return False
+    if x.size == 0:
+        return False
+    if len(x.shape) == 1 and x.shape[0] <= 1:
+        return False
+    if len(x.shape) == 2 and x.shape[0] > 1 and x.shape[1] > 1:
+        return False
     return True
 
 
 def mean(x):
-    if check_param(x):
-        sum = 0
-        for m, n in enumerate(x):
-            sum += n
-        else:
-            return sum / (m + 1)
+    sum = 0
+    for m, n in enumerate(x):
+        sum += n
     else:
-        return None
+        return sum / (m + 1)
 
 
 def var(x):
-    if check_param(x):
-        avg = mean(x)
-        sum = 0
-        for m, n in enumerate(x):
-            sum += pow(n - avg, 2)
-        else:
-            return sum / m
+    avg = mean(x)
+    sum = 0
+    for m, n in enumerate(x):
+        sum += pow(n - avg, 2)
     else:
-        return None
+        return sum / m
 
 
 def std(x):
-    if check_param(x):
-        return math.sqrt(var(x))
-    else:
-        return None
+    return math.sqrt(var(x))
+
 
 def zscore(x):
-    """Computes the normalized version of a non-empty numpy.array using the z-score standardization.
+    """Computes the normalized version of a non-empty numpy.array
+        using the z-score standardization.
     Args:
     x: has to be an numpy.array, a vector.
     Return:
@@ -45,11 +47,14 @@ def zscore(x):
     Raises:
     This function shouldn’t raise any Exception.
     """
+    if not check_param(x):
+        return None
     x_p = (x - mean(x)) / std(x)
     return x_p
 
-X = np.array([[0],[ 15],[ -9],[ 7],[ 12],[ 3],[ -21]])
+
+X = np.array([[0], [15], [-9], [7], [12], [3], [-21]])
 print(zscore(X))
 
-Y = np.array([[2],[ 14],[ -13],[ 5],[ 12],[ 4],[ -19]])
+Y = np.array([[2], [14], [-13], [5], [12], [4], [-19]])
 print(zscore(Y))
