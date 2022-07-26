d2a34935c70eeb8ab1eca225426265780077ad56
cnstll <calle@student.42.fr>
2022-07-26 16:56:43 +0200

    Creating vandermonde matrix

---
+++ ml_02/ex07/polynomial_model.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+def check_param(x, power):
+    if not isinstance(x, np.ndarray) or x.size == 0:
+        return False
+    if not isinstance(power, int) or power < 0:
+        return False
+    return True
+
+
+def add_polynomial_features(x, power):
+    """Add polynomial features to vector x by raising
+    its values up to the power given in argument.
+    Args:
+    x: has to be an numpy.array, a vector of dimension m * 1.
+    power: has to be an int, the power up to which the
+    components of vector x are going to be raised.
+    Return:
+    The matrix of polynomial features as a numpy.array,
+    of dimension m * n,
+    containing the polynomial feature values for all training examples.
+    None if x is an empty numpy.array.
+    None if x or power is not of expected type.
+    Raises:
+    This function should not raise any Exception.
+    """
+    if not check_param(x, power):
+        return None
+    if power == 0:
+        return np.array(x**0)
+    mat = np.array(x)
+    for n in range(2, power + 1):
+        vec = np.array(x**n)
+        mat = np.append(mat, vec, axis=1)
+    return mat
+
+
+x = np.arange(1, 6).reshape(-1, 1)
+print(add_polynomial_features(x, 3))
+
+print(add_polynomial_features(x, 6))
+
+y = np.array([])
+print(add_polynomial_features(y, 2))
