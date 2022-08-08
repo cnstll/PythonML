1dacb528ab5dfc1c67ca9aecf049f71f1e6fb3b4
cnstll <calle@student.42.fr>
2022-08-08 15:24:28 +0200

    Encapsulating code example into __main__ context

--- ml_03/ex00/sigmoid.py
+++ ml_03/ex00/sigmoid.py
@@ -27,19 +27,20 @@ def sigmoid_(x):
     return 1 / (1 + np.exp(-x))
 
 
-# Example 1:
-x = np.array([[-4]])
-np.set_printoptions(precision=17)
-print(sigmoid_(x))
-# Output:
-# array([[0.01798620996209156]])
-# Example 2:
-x = np.array([[2]])
-print(sigmoid_(x))
-# Output:
-# array([[0.8807970779778823]])
-# Example 3:
-x = np.array([[-4], [2], [0]])
-print(sigmoid_(x))
-# Output:
-# array([[0.01798620996209156], [0.8807970779778823], [0.5]])
+if __name__ == '__main__':
+    # Example 1:
+    x = np.array([[-4]])
+    np.set_printoptions(precision=17)
+    print(sigmoid_(x))
+    # Output:
+    # array([[0.01798620996209156]])
+    # Example 2:
+    x = np.array([[2]])
+    print(sigmoid_(x))
+    # Output:
+    # array([[0.8807970779778823]])
+    # Example 3:
+    x = np.array([[-4], [2], [0]])
+    print(sigmoid_(x))
+    # Output:
+    # array([[0.01798620996209156], [0.8807970779778823], [0.5]])
